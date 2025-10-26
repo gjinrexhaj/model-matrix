8d8e5948d228fbd9726586be31003ec8a487b46c
gjin <74433905+gjinrexhaj@users.noreply.github.com>
2025-10-26 13:22:57 -0400

    create unimplemented Application class

---
+++ src/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gjin on 10/26/25.
+//
+
+#include "Application.h"
---
+++ src/Application.h
@@ -0,0 +1,15 @@
+//
+// Created by Gjin on 10/26/25.
+// Application class: framework for building applications with ImGui
+//
+
+#pragma once
+#include <imgui.h>
+
+class Application
+{
+
+};
+
+
+#endif //MODEL_MATRIX_APPLICATION_H
